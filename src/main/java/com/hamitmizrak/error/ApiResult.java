e9adbfb7ffc5c38a1090fd97b15d3374a90d4b03
hamitmizrak <hamitmizrak@gmail.com>
2023-06-06 13:21:24 +0300

    Error

---
+++ src/main/java/com/hamitmizrak/error/ApiResult.java
@@ -0,0 +1,44 @@
+package com.hamitmizrak.error;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Data;
+import lombok.extern.log4j.Log4j2;
+import java.util.Date;
+import java.util.Map;
+
+// LOMBOK
+@Data
+@Builder
+@Log4j2
+
+// Backentte sadece içi dolu verileri frontendte göstersin
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResult {
+
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+    private Date createdDate;
+    private Map<String,String> validationErrors;
+
+    //  parametresiz constructor
+    public ApiResult() {
+    }
+
+    //  parametreli constructor
+    public ApiResult(int status, String error, String message, String path) {
+        this.status = status;
+        this.error = error;
+        this.message = message;
+        this.path = path;
+    }
+
+    //  parametreli constructor
+    public ApiResult(int status, String message, String path) {
+        this.status = status;
+        this.message = message;
+        this.path = path;
+    }
+}
---
+++ src/main/java/com/hamitmizrak/error/CustomErrorHandleWebRequest.java
@@ -0,0 +1,59 @@
+package com.hamitmizrak.error;
+
+import com.hamitmizrak.util.FrontEndURL;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+// LOMBOK
+@RequiredArgsConstructor // for injection
+
+// API
+@RestController
+@CrossOrigin(origins = FrontEndURL.FRONTEND_URL)
+public class CustomErrorHandleWebRequest {
+
+    // INJECTION
+    private final ErrorAttributes errorAttributes;
+
+    // Spring gelen hataları ben alıyorum
+    @RequestMapping("/error")
+    public ApiResult handleError(WebRequest webRequest){
+         int status;
+         String message;
+         String path;
+         ApiResult apiResult;
+
+         // Spring +2.3>
+        Map<String,Object> attributes=errorAttributes.getErrorAttributes(
+                webRequest, ErrorAttributeOptions.of(ErrorAttributeOptions.Include.MESSAGE,ErrorAttributeOptions.Include.BINDING_ERRORS)
+        );
+
+        status= (int) attributes.get("status");
+        message= (String) attributes.get("message");
+        path= (String) attributes.get("path");
+        // int status, String message, String path
+        apiResult=new ApiResult(status,message,path);
+
+        //  attributes
+        if(attributes.containsKey("errors")){
+            List<FieldError> fieldErrorList= (List<FieldError>) attributes.get("error");
+            Map<String,String> validation=new HashMap<>();
+            for ( FieldError fieldError: fieldErrorList){
+                validation.put(fieldError.getField(),fieldError.getDefaultMessage());
+            }
+            apiResult.setValidationErrors(validation);
+        }
+
+        return apiResult;
+    }
+}
