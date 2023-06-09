18b8e5e1d5ee376e905c754b771d4e1c421ac8ab
hamitmizrak <hamitmizrak@gmail.com>
2023-06-09 14:11:43 +0300

    Header.jsx and other jsx

---
+++ frontend/src/component/Header.jsx
@@ -0,0 +1,35 @@
+// rcc
+import React, { Component } from 'react'
+
+// CLASS Component
+export default class Header extends Component {
+    constructor(props) {
+        super(props);
+
+        //bind
+
+        //state
+        this.state = {}
+
+        //bind
+    }
+
+    // CDM
+    componentDidMount() { }
+
+    // FUNCTION
+
+    //RENDER
+    render() {
+
+        //RETURN
+        return (
+            // <div>Header</div>
+            //<React.Fragment>Header</React.Fragment>
+            <>
+                Header
+            </>
+        ) //end return
+    } // end render
+}//end clas
+
