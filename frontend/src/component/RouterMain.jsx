45d4c5614bb88b594f87b2abcce792c988974fdd
hamitmizrak <hamitmizrak@gmail.com>
2023-06-12 11:08:15 +0300

    BrowserRouter, HashRouter , Router , Switch, Route

--- frontend/src/component/RouterMain.jsx
+++ frontend/src/component/RouterMain.jsx
@@ -9,11 +9,19 @@ import Footer from './Footer';
 // i18n 
 import { withTranslation } from 'react-i18next';
 
+// HashRouter => Production için kullanın
+// HashRouter:Backtentten gidecek veya geolecek trafiği kapatıyorum kendimi güvence altına alıyorum
+// HashRouter as Router URL=>(http://localhost:3000/#/)
+// BrowserRouter => Test için
+// BrowserRouter: backent içiin network'u bütün detayına kadar bana göster
+// BrowserRouter as Router URL=>(http://localhost:3000/)
+import { HashRouter as Router, Redirect, Route, Switch } from "react-router-dom"
+
 // CLASS Component
 class RouterMain extends Component {
 
-     // Componentteki yeni isim
-   static displayName="Blog_Router"
+    // Componentteki yeni isim
+    static displayName = "Blog_Router"
 
     constructor(props) {
         super(props);
@@ -38,17 +46,28 @@ class RouterMain extends Component {
         return (
             // <div>Header</div>
             //<React.Fragment>Header</React.Fragment>
-            <>
-                <Header/>
-                <br />
-                <Main/>
-                <br />
-                <Footer/>
-            </>
+            <React.Fragment>
+                <Router>
+                    <Header logo="fa-solid fa-blog" homePageLink="http://localhost:3000/index" />
+                    <div className="container">
+                        <Switch>
+                            <Route path="/" exact component={Main}></Route>
+                            <Route path="/index" component={Main}></Route>
+
+                            <Route path="/blog/list" component={Main}></Route>
+                            <Route path="/blog/create" component={Main}></Route>
+                            <Route path="/blog/update/:id" component={Main}></Route>
+                            <Route path="/blog/view/:id" component={Main}></Route>
+                            {/* eğer kullanıcı bad request atarsa(400) ana sayfaya yönlendirsin */}
+                            <Redirect to="/" />
+                        </Switch>
+                    </div>
+                    <Footer copy="&copy; Bütün haklar Saklıdır." />
+                </Router>
+
+            </React.Fragment>
         ) //end return
     } // end render
 }//end clas
-
-
 
 export default withTranslation()(RouterMain) 
